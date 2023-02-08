2168ad6291a279fa68eb5133c2c18a0bcb3390d3
dam <dam@AULA005-PORTATIL1>
2023-02-08 11:14:16 +0100

    1

--- src/remoto/Remoto.java
+++ src/remoto/Remoto.java
@@ -15,6 +15,7 @@ public class Remoto {
      */
     public static void main(String[] args) {
         System.out.println("hola que tal");
+        System.out.println("soy mjpardo");
     }
     
 }
